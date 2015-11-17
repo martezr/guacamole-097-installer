2782bbcb43e546aa3a3ef83bf6b42dab57ad96c5
Martez Reed <martez.reed@greenreedtech.com>
2015-11-17 11:14:03 -0600

    Update nginx configuration

--- guacamole-097-installer.sh
+++ guacamole-097-installer.sh
@@ -182,7 +182,10 @@ sudo cat schema/*.sql | mysql -u root --password=$mysql_password guacamole
  
 # Install Nginx
 sudo yum install -y nginx
- 
+
+# Configure SELinux to support nginx proxying
+setsebool -P httpd_can_network_connect true
+
 # Create directory to store server key and certificate
 sudo mkdir /etc/nginx/ssl
  
@@ -207,10 +210,49 @@ server  {
   ssl  on;
   location  / {
     proxy_buffering  off;
-    proxy_pass  http://localhost:8080/;
+    proxy_pass  http://127.0.0.1:8080/;
   }
 }
 EOF3
+
+sudo cat << EOF4 > /etc/nginx/nginx.conf
+# For more information on configuration, see:
+#   * Official English Documentation: http://nginx.org/en/docs/
+#   * Official Russian Documentation: http://nginx.org/ru/docs/
+
+user nginx;
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+pid /run/nginx.pid;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile            on;
+    tcp_nopush          on;
+    tcp_nodelay         on;
+    keepalive_timeout   65;
+    types_hash_max_size 2048;
+
+    include             /etc/nginx/mime.types;
+    default_type        application/octet-stream;
+
+    # Load modular configuration files from the /etc/nginx/conf.d directory.
+    # See http://nginx.org/en/docs/ngx_core_module.html#include
+    # for more information.
+    include /etc/nginx/conf.d/*.conf;
+
+}
+EOF4
+
  
 # Restart nginx service
 sudo systemctl restart nginx
